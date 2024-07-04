87f21494fb9d61bb6fffa700d194b82fbed45308
replacementorange <notnenar@proton.me>
2024-07-04 04:33:17 +0300

    4.4: Helper Functions and Unit Tests, step 2

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,17 @@
+// dummy that returns 1
 const dummy = (blogs) => {
     return 1
   }
-  
+
+// sum of numbers in array
+const totalLikes = (blogs) => {
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
+    const sumOfLikes = blogs.reduce((sum, blog) => {
+      return sum +  blog.likes
+    }, 0)
+    return sumOfLikes
+  }
+
   module.exports = {
-    dummy
+    dummy, totalLikes
   }
